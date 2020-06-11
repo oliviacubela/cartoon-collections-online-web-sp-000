078acec059117496f6b3dfd9b518587b339a03b5
Olivia Cubela <oliviacubela@gmail.com>
2020-06-11 16:28:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,10 +10,16 @@ def summon_captain_planet(veggies)
   end
 end
 
-def summon_captain_planet(veggies)
-    veggies.collect do |call|
-    call.capitalize + "!"
+def long_planteer_calls(long_planteer_calls)
+  answer = false
+  long_planteer_calls.each do |call|
+    if call.length > 4
+      answer = true
+    end
   end
+    answer
+end
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
